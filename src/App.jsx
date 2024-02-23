e19d0299a48dbfe70f10552fa6b015bef26dbe9b
Prashant Tomar <prashanttomar765@gmail.com>
2024-02-23 22:42:38 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+function App() {
+  return (
+    <>
+      <div className="flex items-center justify-center h-screen bg-black">
+        <div className="p-6 rounded-md shadow-md bg-slate-500">
+          <form>
+            <div class="mb-6">
+              <label
+                for="large-input"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Title
+              </label>
+              <input
+                type="text"
+                id="large-input"
+                class="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              />
+            </div>
+            <label
+              for="message"
+              class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Add Description
+            </label>
+            <textarea
+              id="message"
+              rows="4"
+              class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-4"
+              placeholder="Write your message here..."
+            ></textarea>
+            <button
+              type="submit"
+              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Create Todo
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
