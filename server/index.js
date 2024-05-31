55f12c51ccd4497aaf4fc327d605cc97c818fac1
Prashant Tomar <prashanttomar765@gmail.com>
2024-06-01 01:17:25 +0530

    added dependecies

--- server/index.js
+++ server/index.js
@@ -1,11 +1,26 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import { PORT, mongoDbUrl } from "./config.js";
+import todoRoutes from "./routes/todoRoutes.js"
 
 const app = express();
+
+// Middlewares
 app.use(cors());
 app.use(express.json());
+app.use('/todos', todoRoutes)
 
-app.listen(3000, () => {
-    console.log("Server be likee meri koi galti nhi hai")
-})
+mongoose
+  .connect(
+    mongoDbUrl
+  )
+  .then(() => {
+    console.log("Connected to Database");
+    app.listen(PORT, () => {
+      console.log("Server ki koi galti nhi hai");
+    });
+  })
+  .catch(() => {
+    console.log("failed to connect to Database");
+  });
