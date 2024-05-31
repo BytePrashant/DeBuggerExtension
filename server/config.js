b4eea05df3161db8b509377edf58782b04a00fdd
Prashant Tomar <prashanttomar765@gmail.com>
2024-06-01 01:16:40 +0530

    added config file

---
+++ server/config.js
@@ -0,0 +1,5 @@
+export const PORT = 3000;
+
+export const mongoDbUrl = "mongodb+srv://prashanttomar765:MongoTodoApp@todoserver.tyxxuod.mongodb.net/Todos?retryWrites=true&w=majority&appName=TodoServer"
+
+
