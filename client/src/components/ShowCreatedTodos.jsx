61b29d9dc6efd4f9f5c757389598a4406cb26c05
Prashant Tomar <prashanttomar765@gmail.com>
2024-06-04 01:39:14 +0530

    todos are getting render based on status

--- client/src/components/ShowCreatedTodos.jsx
+++ client/src/components/ShowCreatedTodos.jsx
@@ -6,11 +6,10 @@ import {
   faPenToSquare,
   faArrowRight,
 } from "@fortawesome/free-solid-svg-icons";
-import DataContext from "../utils/DataContext";
+import TodoContext from "../utils/TodoContext";
 
 const ShowCreatedTodos = () => {
-  const { todos, setTodos, status, setStatus } = useContext(DataContext);
-
+  const { todos } = useContext(TodoContext);
 
   //   handle delete
   const handleDelete = (id) => {
@@ -23,7 +22,6 @@ const ShowCreatedTodos = () => {
         console.error("There was an error deleting the todo:", error);
       });
   };
-
   // update todo
   const handleEdit = (id) => {
     axios
@@ -36,41 +34,31 @@ const ShowCreatedTodos = () => {
       });
   };
 
-  // send todo to in progress component
-  // const sendToInProgress = (status) => {
-  //   setStatus("inPending");
-  // };
-
-  console.log("Mai ShowTodo component")
-
   return (
     <div className="container h-full p-2 overflow-auto">
       {todos.length > 0 ? (
         todos
           .filter((todo) => todo.status === "created")
           .map((todo) => (
-            <div key={todo._id} className="border-2 flex">
-              <div className="flex flex-col p-2 w-9/12">
+            <div key={todo._id} className="border-2 flex w-full">
+              <div className="flex flex-col p-2 w-4/5">
                 <h2 className="font-bold text-xl">{todo.title}</h2>
                 <p className="font-extralight text-sm truncate overline decoration-sky-500">
                   {todo.description}
                 </p>
               </div>
-              <div className="flex items-center justify-between p-2 w-3/12">
+              <div className="flex items-center justify-center w-1/5">
                 <FontAwesomeIcon
+                  className="p-1 w-full"
                   onClick={() => handleDelete(todo._id)}
                   icon={faTrash}
                   style={{ color: "#e69b19" }}
                 />
                 <FontAwesomeIcon
-                  onClick={() => handleEdit(todo._id)}
-                  icon={faPenToSquare}
-                  style={{ color: "#B197FC" }}
-                />
-                <FontAwesomeIcon
+                  className="p-1 w-full"
                   onClick={() => {
                     sendToInProgress();
-                    setTodos(todos)
+                    setTodos(todos);
                   }}
                   icon={faArrowRight}
                   style={{ color: "#FFD43B" }}
--- client/src/components/ShowPendingTodo.jsx
+++ client/src/components/ShowPendingTodo.jsx
@@ -1,18 +1,42 @@
-import React, { useEffect, useContext } from "react";
-import axios from "axios";
+import React, { useContext } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faTrash,
-  faPenToSquare,
-  faArrowRight,
-} from "@fortawesome/free-solid-svg-icons";
-import DataContext from "../utils/DataContext";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import TodoContext from "../utils/TodoContext";
 
 const ShowPendingTodos = () => {
+  const { todos } = useContext(TodoContext);
 
   return (
     <div className="container h-full p-2 overflow-auto">
-      
+      {todos.length > 0 ? (
+        todos
+          .filter((todo) => todo.status === "pending")
+          .map((todo) => (
+            <div key={todo._id} className="border-2 flex w-full">
+              <div className="flex flex-col p-2 w-4/5">
+                <h2 className="font-bold text-xl">{todo.title}</h2>
+                <p className="font-extralight text-sm truncate overline decoration-sky-500">
+                  {todo.description}
+                </p>
+              </div>
+              <div className="flex items-center justify-center w-1/5">
+                <FontAwesomeIcon
+                  className="p-1 w-full"
+                  onClick={() => {
+                    sendToInProgress();
+                    setTodos(todos);
+                  }}
+                  icon={faArrowRight}
+                  style={{ color: "#FFD43B" }}
+                />
+              </div>
+            </div>
+          ))
+      ) : (
+        <p className="border-2 h-full flex items-center justify-center font-semibold text-xl">
+          No bugs for today?
+        </p>
+      )}
     </div>
   );
 };
