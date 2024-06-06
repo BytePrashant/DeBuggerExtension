160827f6ae6dd494d6e7d51cd945d1f39bbf4a3d
Prashant Tomar <prashanttomar765@gmail.com>
2024-06-06 17:58:14 +0530

    added todo context functionality

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -20,8 +20,46 @@ const Hero = () => {
       });
   }, []);
 
+  // change status to pending
+  const sendToPending = async (todo) => {
+    try {
+      const updatedTodo = { ...todo, status: "pending" };
+      setTodos((prevTodos) =>
+        prevTodos.map((prevTodo) =>
+          prevTodo._id === todo._id ? updatedTodo : prevTodo
+        )
+      );
+      await axios.put(`http://localhost:3000/todos/${todo._id}`, updatedTodo); // Send a PUT request
+    } catch (error) {
+      console.error("Error updating todo status:", error);
+    }
+  };
+
+  // change status to completed
+  const sendToCompleted = async (todo) => {
+    try {
+      const updatedTodo = { ...todo, status: "completed" }; // Create a copy with updated status
+      setTodos((prevTodos) =>
+        prevTodos.map((prevTodo) =>
+          prevTodo._id === todo._id ? updatedTodo : prevTodo
+        )
+      );
+      await axios.put(`http://localhost:3000/todos/${todo._id}`, updatedTodo);
+    } catch (error) {
+      console.error("Error updating todo status:", error);
+    }
+  };
+
+  // handle Delete function
+  const handleDelete = async (id) => {
+    axios.delete(`http://localhost:3000/todos/${id}`);
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo._id !== id));
+  };
+
   return (
-    <TodoContext.Provider value={{ todos, setTodos }}>
+    <TodoContext.Provider
+      value={{ todos, setTodos, sendToPending, sendToCompleted, handleDelete }}
+    >
       <div className="Container flex flex-row p-16 h-lvh items-center">
         <BoxCreated />
         <BoxPending />
