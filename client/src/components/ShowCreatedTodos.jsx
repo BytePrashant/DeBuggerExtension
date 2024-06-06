dcb1103bc686e45dbb8945d084bb7504ac6973e7
Prashant Tomar <prashanttomar765@gmail.com>
2024-06-06 17:59:08 +0530

    Show created todo works fine

--- client/src/components/ShowCreatedTodos.jsx
+++ client/src/components/ShowCreatedTodos.jsx
@@ -1,45 +1,15 @@
 import React, { useContext } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faTrash,
-  faPenToSquare,
-  faArrowRight,
-} from "@fortawesome/free-solid-svg-icons";
+import { faTrash, faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import TodoContext from "../utils/TodoContext";
-import axios from 'axios'
 
 const ShowCreatedTodos = () => {
-  const { todos, setTodos } = useContext(TodoContext);
-
-  // handle Delete function
-  const handleDelete = async(id) => {
-    axios
-    .delete(`http://localhost:3000/todos/${id}`)
-    setTodos((prevTodos) => prevTodos.filter((todo) => todo._id !== id));
-  }
-
-  // handle Status change function
-  const sendToInProgress = async (todo) => {
-    try {
-      const updatedTodo = { ...todo, status: "pending" }; // Create a copy with updated status
-      const response = await axios.put(`http://localhost:3000/todos/${todo._id}`, updatedTodo); // Send a PUT request
-  
-      if (response.ok) {
-        // Update the local state with the updated todo (optional)
-        setTodos(prevTodos => prevTodos.map(prevTodo => prevTodo._id === todo._id ? updatedTodo : prevTodo));
-      } else {
-        throw new Error("Error updating todo status");
-      }
-    } catch (error) {
-      console.error("Error updating todo status:", error);
-      // Handle errors (e.g., display an error message to the user)
-    }
-  };
+  const { todos, sendToPending, handleDelete } = useContext(TodoContext);
 
   return (
     <div className="container h-full p-2 overflow-auto">
-     
-        {todos
+      {todos.length > 0 ? (
+        todos
           .filter((todo) => todo.status === "created")
           .map((todo) => (
             <div key={todo._id} className="border-2 flex w-full">
@@ -59,7 +29,7 @@ const ShowCreatedTodos = () => {
                 <FontAwesomeIcon
                   className="p-1 w-full"
                   onClick={() => {
-                    sendToInProgress(todo);
+                    sendToPending(todo);
                   }}
                   icon={faArrowRight}
                   style={{ color: "#FFD43B" }}
@@ -67,7 +37,11 @@ const ShowCreatedTodos = () => {
               </div>
             </div>
           ))
-      }
+      ) : (
+        <p className="border-2 h-full flex items-center justify-center font-semibold text-xl">
+          No Bugs for today 😁
+        </p>
+      )}
     </div>
   );
 };
