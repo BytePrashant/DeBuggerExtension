248113d532a65c22a8818151ad6d048f475db7db
Prashant Tomar <prashanttomar765@gmail.com>
2024-06-09 12:34:12 +0530

    live server url on PORT

--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
-export const PORT = 3000;
+export const PORT = 'https://debuggerextension.onrender.com/';
 
 export const mongoDbUrl = "mongodb+srv://prashanttomar765:MongoTodoApp@todoserver.tyxxuod.mongodb.net/Todos?retryWrites=true&w=majority&appName=TodoServer"
 
